c305f6a06d4504fbc6d2260beae220fffce26888
shams220 <103259015+shams220@users.noreply.github.com>
2025-10-06 14:15:14 +0530

    new changes

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const cors = require('cors')
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: ["https://coderevai.netlify.app"], // or "*" for testing
+}))
 
 const aiRouter = require('./routes/route')
 
